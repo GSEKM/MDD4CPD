a4d28a43f6865bae0e0de1f9a51801843c8ba4f1
rzfzr <capitao.rafa@gmail.com>
2024-04-25 15:54:07 -0300

    Initiating types

---
+++ src/types.d.ts
@@ -0,0 +1,16 @@
+
+// type Method = {
+//     name: string;
+//     params: Param[];
+//     returns: Param;
+// }
+
+export type NodeData = {
+    name: string;
+    color: string;
+    extras: any;
+    methods: String[];
+    ins: String[];
+    outs: String[];
+}
+
