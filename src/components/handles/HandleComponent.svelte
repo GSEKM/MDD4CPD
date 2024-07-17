b25d8f97af28e234505bf508211d25980bf95e64
Gabriel Marques Gallo <80481877+Gallo-10@users.noreply.github.com>
2024-07-17 16:36:24 -0300

    Styled handle

--- src/components/handles/HandleComponent.svelte
+++ src/components/handles/HandleComponent.svelte
@@ -1,28 +1,36 @@
 <script lang="ts">
-    import { Position, Handle as SvelteHandle } from "@xyflow/svelte";
-  
-    export let handleInfo: {
-      id: string;
-      position: Position;
-      type: "source" | "target";
-      isConnectable: boolean;
-      edge: string;
-      index: number;
-    };
-    console.log("handle info: "+handleInfo.edge )
-    const offset = 45 + handleInfo.index * 30;
-  </script>
-  
-  <div id={handleInfo.id}>
-    <p style={`top: ${offset}px; right: 20px; margin: 0; line-height: 20px;`}>
-      {handleInfo.edge}
-    </p>
-    <SvelteHandle
-      id={handleInfo.id}
-      position={handleInfo.position}
-      isConnectable={handleInfo.isConnectable}
-      type={handleInfo.type}
-      style={`top: ${offset}px;`} 
-    />
-  </div>
-  
+  import { Position, Handle as SvelteHandle } from "@xyflow/svelte";
+  export let handleInfo: {
+    id: string;
+    position: Position;
+    type: "source" | "target";
+    isConnectable: boolean;
+    edge: string;
+    index: number;
+  };
+
+  console.log("handle info: " + handleInfo.edge);
+</script>
+
+<style>
+  .container {
+    position: relative;
+    padding: 10px;
+    border: 1px solid #00000017;
+  }
+  .handle {
+    position: absolute;
+  }
+</style>
+
+<div id={handleInfo.id} class="container">
+  <p>{handleInfo.edge}</p>
+  <SvelteHandle
+    class="handle {handleInfo.edge === 'left' ? 'handle-left' : handleInfo.edge === 'right' ? 'handle-right' : handleInfo.edge === 'top' ? 'handle-top' : 'handle-bottom'}"
+    id={handleInfo.id}
+    position={handleInfo.position}
+    isConnectable={handleInfo.isConnectable}
+    type={handleInfo.type}
+    style="background-color: #fff; border: 1px solid #000; border-radius: 50%; width: 10px; height: 10px;"
+  />
+</div>
